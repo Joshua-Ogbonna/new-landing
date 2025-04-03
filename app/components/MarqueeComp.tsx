8204bd22f15e7016dfef831b681aa7d4efe3a61c
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-04-03 10:19:06 +0100

    Marq1 almost done

--- app/components/MarqueeComp.tsx
+++ app/components/MarqueeComp.tsx
@@ -7,8 +7,12 @@ interface MarqueeProps {
 const MarqueeComp: React.FC<MarqueeProps> = ({background,textColor}) => {
   return (
 <Marquee
-  style={{ backgroundColor: background, color: textColor }}
-  className="font-bold py-2 md:py-5 text-lg md:text-5xl"
+  style={{ 
+    backgroundColor: background, 
+    color: textColor,
+ 
+  }}
+  className="font-bold py-2 md:py-5 text-lg md:text-5xl skew-x-[-5deg] drop-shadow-[2px_2px_0px_rgba(0,0,0,0.2)] "
   speed={120} 
   gradient={false}
   direction="right"
